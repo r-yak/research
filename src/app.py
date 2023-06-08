2f191b6b537bd11e3c341ef69bf8914b70745aa5
Hepheir <hepheir@gmail.com>
2023-06-08 21:47:43 +0900

    feat(src/app): 이미지로부터 색깔 6개를 뽑아 출력하는 기능 구현

--- src/app.py
+++ src/app.py
@@ -1,4 +1,10 @@
+import typing
+
 import cv2
+import colorgram
+import numpy
+
+from PIL import Image
 
 
 KEY_CODE_ESC = 27
@@ -9,8 +15,8 @@ capture: cv2.VideoCapture
 def setup():
     global capture
     capture = cv2.VideoCapture(0)
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 480)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+    capture.set(cv2.CAP_PROP_FRAME_WIDTH, 240)
     while cv2.waitKey(33) != KEY_CODE_ESC:
         loop()
     capture.release()
@@ -22,6 +28,24 @@ def loop():
     if not ret:
         return
     cv2.imshow('CAMERA', frame)
+    show_colors(extract_color(frame))
+
+
+def extract_color(frame: numpy.ndarray) -> typing.List[colorgram.Color]:
+    number_of_colors = 6
+    down_sample_dst_size = (64, 64)
+    down_sampled_frame = cv2.resize(frame, down_sample_dst_size)
+    pil_image = Image.fromarray(down_sampled_frame)
+    return colorgram.extract(pil_image, number_of_colors)
+
+
+def show_colors(colors: typing.List[colorgram.Color]) -> None:
+    block_size = 64
+    frame = numpy.zeros(
+        (block_size, block_size * len(colors), 3), dtype=numpy.uint8)
+    for i, color in enumerate(colors):
+        frame[:, block_size*i:block_size*(i+1)] = color.rgb
+    cv2.imshow('COLOR', frame)
 
 
 if __name__ == '__main__':
